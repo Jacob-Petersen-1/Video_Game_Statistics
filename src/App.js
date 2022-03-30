f5bf5765d267401fc5a0c5056fc9f74d9ff406b4
matthew-pryor <pryor.matthew.a@gmail.com>
2022-03-30 14:46:11 -0500

    created async function getAllGames to communicate with the backend + created the searchBar filter function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
 import './App.css';
+import React, { useState, useEffect} from 'react';
 
 function App() {
+
+  const [games, setGames] = useState([]);
+  const [displayGames, setDisplayGames] = useState([])
+
+  useEffect(()=>{
+    getAllGames();
+  }, [])
+
+  async function getAllGames(){
+    let response = await axios.get('https://localhost:7260/api/games');
+    setSongs(response.data)
+    setDisplayGames(response.data)
+    console.log(response.data)
+  }
+
+  const filterGames = (searchGame) => {
+
+    let matchingGames = games.filter((game) => {
+      if(game.name.toLowerCase().includes(searchGame.toLowerCase())){
+
+        return true
+      }
+      else{
+        return false
+      }
+    })
+
+    setDisplayGames(matchingGames)
+  }
+
   return (
     <div className="App">
     
