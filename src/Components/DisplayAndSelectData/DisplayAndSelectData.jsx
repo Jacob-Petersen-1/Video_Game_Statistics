01c7c0b3ce7e38b1917886cc01928d9d5a58e9e7
matthew-pryor <pryor.matthew.a@gmail.com>
2022-03-31 10:04:02 -0500

    conducted additional research on how to make a selectable table and implemented additions into the DisplayAndSelectData component

--- src/Components/DisplayAndSelectData/DisplayAndSelectData.jsx
+++ src/Components/DisplayAndSelectData/DisplayAndSelectData.jsx
@@ -1,6 +1,51 @@
 import React from "react";
 
+class SelectTableComponent extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            List: props.parentEntries,
+            MasterChecked: false,
+            SelectedList: [],
+        };
+    }
 
+    onMasterCheck(e){
+        let tempList = this.state.List;
+        tempList.map((game)=>(game.selected = e.target.checked))
+
+        this.setState({
+            MasterChecked: e.target.checked,
+            List: tempList,
+            SelectedList: this.state.List.filter((e) => e.selected),
+        });
+    }
+
+    onGameCheck(e, item){
+        let tempList = this.state.List;
+        tempList.map((game)=>{
+            if(game.id === item.id){
+                game.selected = e.target.checked;
+            }
+            return game
+        });
+
+        const totalItems = this.state.List.length;
+        const totalCheckedItems = tempList.filter((e)=>e.selected).length;
+
+        this.setState({
+            MasterChecked: totalItems === totalCheckedItems,
+            List: tempList,
+            SelectedList: this.state.List.filter((e)=>e.selected),
+        });
+    }
+
+    getSelectedRows(){
+        this.setState({
+            SelectedList: this.state.List.filter((e)=>e.selected),
+        })
+    }
+}
 
 const DisplayAndSelectData = (props) => {
     return ( 
