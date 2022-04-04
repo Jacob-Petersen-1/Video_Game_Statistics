77123cc091b37f3f72b4907321a87705f53b0de8
matthew-pryor <pryor.matthew.a@gmail.com>
2022-04-04 14:31:10 -0500

    made use of the useState in order to clear the previous search from the search bar

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -8,12 +8,18 @@ const SearchBar = (props) => {
     function handleSubmit(entry){
         entry.preventDefault();
         props.filterGames(searchGame)
+        setSearchGame('')
     }
 
     return ( 
         <div className="searchbar-container"> 
             <form onSubmit={handleSubmit}>
-                    <input value={searchGame} onChange={(entry) => setSearchGame(entry.target.value)} type="text" placeholder="Search..." className="input"></input>
+                    <input 
+                    value={searchGame} 
+                    onChange={(entry) => setSearchGame(entry.target.value)} 
+                    type="text" 
+                    placeholder="Search..." 
+                    className="input"></input>
                     <button type="text" class="submit" className="search-button">Search</button>
             </form>
         </div>
