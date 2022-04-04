c76107d0e6f9b92bbcfec57b2ca75c2c6e56c2c8
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2022-04-04 12:50:55 -0600

    Adjust search results table css + Adjust paginate location

--- src/Components/DisplayAndSelectData/DisplayAndSelectData.jsx
+++ src/Components/DisplayAndSelectData/DisplayAndSelectData.jsx
@@ -1,61 +1,66 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ReactPaginate from "react-paginate";
-import "./DisplayAndSelectData.css"
+import "./DisplayAndSelectData.css";
 
 const DisplayAndSelectData = (props) => {
+  const [pageNumber, setPageNumber] = useState(0);
+  const gamesPerPage = 20;
+  const games = props.games;
+  const displayedGames = pageNumber * gamesPerPage;
+  const page = Math.ceil(games.length / gamesPerPage);
+  const nextPage = ({ selected }) => {
+    setPageNumber(selected);
+  };
 
-    const [pageNumber, setPageNumber] = useState(0);
-    const gamesPerPage = 20;
-    const games = props.games;
-    const displayedGames = pageNumber * gamesPerPage;
-    const page = Math.ceil(games.length / gamesPerPage);
-    const nextPage = ({selected}) => {setPageNumber(selected);}
-
-    return ( 
-        <div>
-            <div>
-                <h2>Search Results:</h2>
-            </div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Rank</th>
-                        <th>Title</th>
-                        <th>Platform</th>
-                        <th>Release Year</th>
-                        <th>Genre</th>
-                        <th>Publisher</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    {games.slice(displayedGames, displayedGames + gamesPerPage).map((game, index)=>{
-                        return(
-                            <tr key={index}>
-                                <td>{game.rank}</td>
-                                <td>{game.name}</td>
-                                <td>{game.platform}</td>
-                                <td>{game.year}</td>
-                                <td>{game.genre}</td>
-                                <td>{game.publisher}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-                <div><ReactPaginate 
-                className="pagination"
-                previousLabel="Previous" 
-                nextLabel="Next" 
-                pageCount={page} 
-                onPageChange={nextPage}
-                />
-                </div>
-
-
+  return (
+    <div>
+      <div>
+        <h2>Search Results:</h2>
+      </div>
+      <div className="search-container">
+        <div className="pagination-container">
+          <div>
+            <ReactPaginate
+              className="pagination"
+              previousLabel="<PREV"
+              nextLabel="NEXT>"
+              pageCount={page}
+              onPageChange={nextPage}
+            />
+          </div>
         </div>
-        
-     );
-}
- 
+        <table className="search-table">
+          <thead>
+            <tr className="header">
+              <th>Rank</th>
+              <th>Title</th>
+              <th>Platform</th>
+              <th>Release Year</th>
+              <th>Genre</th>
+              <th>Publisher</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {games
+              .slice(displayedGames, displayedGames + gamesPerPage)
+              .map((game, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{game.rank}</td>
+                    <td>{game.name}</td>
+                    <td>{game.platform}</td>
+                    <td>{game.year}</td>
+                    <td>{game.genre}</td>
+                    <td>{game.publisher}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
 export default DisplayAndSelectData;
